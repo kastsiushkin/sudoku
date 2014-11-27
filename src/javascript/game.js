c6d1bf41f29585eacda2c32ed932dbe803936e17
kastsiushkin <alexkastsiushkin@gmail.com>
2014-11-26 18:48:17 -0800

    Refactor code to save cell-object in the game array instead of row numbers

--- src/javascript/game.js
+++ src/javascript/game.js
@@ -9,7 +9,7 @@ var Sudoku = (function($) {
 		,time
 		,timeInput
 		,reset
-		,gameArray = [];
+		,gameArray;
 
 	return {
 		init: function (args) {
@@ -22,12 +22,15 @@ var Sudoku = (function($) {
 
 			delegateEvents();
 
+			generateEmptyArray();
 			generateInitialGame();
 			renderGame(gameArray);
 		}
 	}
 
-	//// Helper functions //////
+	/* ****************************
+		Helper functions
+	* ****************************/
 
 	function createGameBoard() {
 		game = createElement('table');
@@ -35,16 +38,13 @@ var Sudoku = (function($) {
 		for (var i = 0; i < 9; i++) {
 			var row = createElement('tr');
 			game.append(row);
-			var colArray = [];
 			for (var j = 0; j < 9; j++) {
 				var cell = createElement('td');
 				cell.attr('id', 'cell-' + i + '-' + j);
 				cell.attr('data-row', i);
 				cell.attr('data-col', j);
-				colArray.push(-1);
 				row.append(cell);
 			}
-			gameArray.push(colArray);
 		}
 		appendElement(gameBoard, game);
 	};
@@ -81,13 +81,14 @@ var Sudoku = (function($) {
 		addIdAttribute(reset, 'reset');
 		toolbar.append(reset);
 
-
+		//Add toolbar to the gameboard
 		appendElement(gameBoard, toolbar);
 	};
 
 	function delegateEvents () {
 		game.bind('click', cellClick);
 		keypad.bind('click', keyClick);
+		reset.bind('click', resetClick);
 	};
 
 	function createElement (element) {
@@ -102,13 +103,26 @@ var Sudoku = (function($) {
 		container.attr('id', id);
 	}
 
+	//Update dom of current cell
 	function updateCurrentCell (value) {
-		var row = +currentCell.dataset.row,
-			col = +currentCell.dataset.col;
-		gameArray[row][col] = value;
 		$(currentCell).text(value);
-	}
+	};
 
+	//Update js array after user selection
+	function updateGameArray (value) {
+		var position = getRowAndColOfCurrentCell();
+		gameArray[position.row][position.col] = (cell(position.row, position.col, value));
+	};
+
+	//Returns row and col of currently selected cell
+	function getRowAndColOfCurrentCell () {
+		return {
+			row: +currentCell.dataset.row,
+			col: +currentCell.dataset.col
+		}
+	};
+
+	//Check current selection
 	function checkSelection (choice) {
 		var row = +currentCell.dataset.row,
 			col = +currentCell.dataset.col;
@@ -116,6 +130,186 @@ var Sudoku = (function($) {
 			markError();
 		} else {
 			markOk();
+		}
+	}
+
+	/* ****************************
+		Event handlers
+	* ****************************/
+	function cellClick (event) {
+		if ( !timer ) {
+			startTime();
+		}
+		//TO-DO refactor this code
+		$(currentCell).removeClass('active');
+		currentCell = event.target;
+		$(currentCell).addClass('active');
+		if ( currentCell.className.indexOf('locked') !== -1 ) {
+			return false;
+		}
+		var bottomLimit = event.currentTarget.clientHeight;
+		var currentBottom = currentCell.offsetTop + currentCell.clientHeight * 4;
+		var leftLimit = event.currentTarget.clientWidth;
+		var currentleft = currentCell.offsetLeft + currentCell.clientWidth * 4;
+		var top, left;
+		if ( currentBottom < bottomLimit ) {
+			top = currentCell.offsetTop + currentCell.clientHeight;
+		} else {
+			top = currentCell.offsetTop - currentCell.clientHeight * 3;
+		}
+		if ( currentleft < leftLimit ) {
+			left = currentCell.offsetLeft + currentCell.clientWidth;
+		} else {
+			left = currentCell.offsetLeft - currentCell.clientWidth * 3;
+		}
+
+		console.log(event.currentTarget.clientHeight, currentCell.offsetTop + currentCell.clientHeight * 4);	
+
+		keypad.css('opacity', 0);
+		keypad.css('display', 'none');
+		keypad.css('top', top);
+		keypad.css('left', left);
+		keypad.css('display', 'block');
+		keypad.animate({
+			opacity: 1
+		}, 200);
+	};
+
+	function keyClick (event) {
+		$(currentCell).removeClass('active');
+		event.stopPropagation();
+		var choice = event.target.innerHTML;
+		keypad.css('display', 'none');
+		updateCurrentCell(+choice);
+		updateGameArray(+choice);
+		checkSelection(+choice);
+	};
+
+	function resetClick (event) {
+		checkGame();
+	};
+
+	/* ****************************
+		Create js representation of the board
+	* ****************************/
+	function addValue (cell) {
+		gameArray[cell.row][cell.col] = cell;
+	};
+
+	function generateEmptyArray () {
+		gameArray = [];
+		for (var i = 0; i < 9; i++) {
+			gameArray[i] = [];
+			for (var j = 0; j < 9; j++) {
+				gameArray[i][j] = undefined;
+			}
+		}
+	};
+
+	function generateInitialGame () {
+		//Row 1
+		addValue(cell(0, 0, 5, true, true));
+		addValue(cell(0, 1, 3, true, true));
+		addValue(cell(0, 4, 7, true, true));
+		//Row 2
+		addValue(cell(1, 0, 6, true, true));
+		addValue(cell(1, 3, 1, true, true));
+		addValue(cell(1, 4, 9, true, true));
+		addValue(cell(1, 5, 5, true, true));
+		//Row 3
+		addValue(cell(2, 1, 9, true, true));
+		addValue(cell(2, 2, 8, true, true));
+		addValue(cell(2, 7, 6, true, true));
+		//Row 4
+		addValue(cell(3, 0, 8, true, true));
+		addValue(cell(3, 4, 6, true, true));
+		addValue(cell(3, 8, 3, true, true));
+		//Row 5
+		addValue(cell(4, 0, 4, true, true));
+		addValue(cell(4, 3, 8, true, true));
+		addValue(cell(4, 5, 3, true, true));
+		addValue(cell(4, 8, 1, true, true));
+		//Row 6
+		addValue(cell(5, 0, 7, true, true));
+		addValue(cell(5, 4, 2, true, true));
+		addValue(cell(5, 8, 6, true, true));
+		//Row 7
+		addValue(cell(6, 1, 6, true, true));
+		addValue(cell(6, 6, 2, true, true));
+		addValue(cell(6, 7, 8, true, true));
+		//Row 8
+		addValue(cell(7, 3, 4, true, true));
+		addValue(cell(7, 4, 1, true, true));
+		addValue(cell(7, 5, 9, true, true));
+		addValue(cell(7, 8, 5, true, true));
+		//Row 9
+		addValue(cell(8, 4, 8, true, true));
+		addValue(cell(8, 7, 7, true, true));
+		addValue(cell(8, 8, 9, true, true));
+	}
+
+	function cell (row, col, number, valid, locked) {
+		return {
+			row: row,
+			col: col,
+			value: number,
+			valid: valid || true,
+			locked: locked || false
+		}
+	}
+
+	/* ****************************
+		Rendering 
+	* ****************************/
+
+	//Render the initial game and lock cells
+	function renderGame (array) {
+		for (var i = 0; i < 9; i++) {
+			for (var j = 0; j < 9; j++) {
+				var cell = array[i][j];
+				if ( cell !== undefined ) {
+					var domCell = getCellById(cell.row, cell.col);
+					domCell.text(cell.value);
+					domCell.addClass('locked');
+					domCell.attr('data-value', cell.value);
+				}
+			}
+		}
+	};
+
+	function markError (rowIndex, colIndex) {
+		$(currentCell).css('color', 'red');
+	};
+
+	function markOk (rowIndex, colIndex) {
+		$(currentCell).css('color', 'black');
+	}
+
+	function getCellById (row, col) {
+		return $('#cell-' + row + '-' + col);
+	}
+
+	function getIds (row, value) {
+		
+	}
+
+	function renderNewTime (timeString) {
+		$('#time-input').text(timeString);
+	}
+
+	/* ****************************
+		Check the game 
+	* ****************************/
+
+	//Check full board
+	function checkGame (argument) {
+		console.log(gameArray[0]);
+		//Copy array
+		var arrayToCheck = gameArray[0].sort();
+		for (var i = 0; i < 8; i++) {
+			if ( gameArray[0][i] === gameArray[0][i+1] && gameArray[0][i] !== -1 ) {
+				getIds();
+			}
 		}
 	}
 
@@ -173,134 +367,9 @@ var Sudoku = (function($) {
 		return startIndex;
 	}
 
-	/* Event handlers  */
-	function cellClick (event) {
-		if ( !timer ) {
-			startTime();
-		}
-		//TO-DO refactor this code
-		$(currentCell).removeClass('active');
-		currentCell = event.target;
-		$(currentCell).addClass('active');
-		if ( currentCell.className.indexOf('locked') !== -1 ) {
-			return false;
-		}
-		var bottomLimit = event.currentTarget.clientHeight;
-		var currentBottom = currentCell.offsetTop + currentCell.clientHeight * 4;
-		var leftLimit = event.currentTarget.clientWidth;
-		var currentleft = currentCell.offsetLeft + currentCell.clientWidth * 4;
-		var top, left;
-		if ( currentBottom < bottomLimit ) {
-			top = currentCell.offsetTop + currentCell.clientHeight;
-		} else {
-			top = currentCell.offsetTop - currentCell.clientHeight * 3;
-		}
-		if ( currentleft < leftLimit ) {
-			left = currentCell.offsetLeft + currentCell.clientWidth;
-		} else {
-			left = currentCell.offsetLeft - currentCell.clientWidth * 3;
-		}
-
-		console.log(event.currentTarget.clientHeight, currentCell.offsetTop + currentCell.clientHeight * 4);	
-
-		keypad.css('opacity', 0);
-		keypad.css('display', 'none');
-		keypad.css('top', top);
-		keypad.css('left', left);
-		keypad.css('display', 'block');
-		keypad.animate({
-			opacity: 1
-		}, 200);
-	};
-
-	function keyClick (event) {
-		$(currentCell).removeClass('active');
-		event.stopPropagation();
-		var choice = event.target.innerHTML;
-		keypad.css('display', 'none');
-		updateCurrentCell(+choice);
-		checkSelection(+choice);
-	};
-
-	/* Game Creation */
-	function addValue (row, col, value) {
-		gameArray[row][col] = value;
-	}
-
-	function generateInitialGame () {
-		//Row 1
-		addValue(0, 0, 5);
-		addValue(0, 1, 3);
-		addValue(0, 4, 7);
-		//Row 2
-		addValue(1, 0, 6);
-		addValue(1, 3, 1);
-		addValue(1, 4, 9);
-		addValue(1, 5, 5);
-		//Row 3
-		addValue(2, 1, 9);
-		addValue(2, 2, 8);
-		addValue(2, 7, 6);
-		//Row 4
-		addValue(3, 0, 8);
-		addValue(3, 4, 6);
-		addValue(3, 8, 3);
-		//Row 5
-		addValue(4, 0, 4);
-		addValue(4, 3, 8);
-		addValue(4, 5, 3);
-		addValue(4, 8, 1);
-		//Row 6
-		addValue(5, 0, 7);
-		addValue(5, 4, 2);
-		addValue(5, 8, 6);
-		//Row 7
-		addValue(6, 1, 6);
-		addValue(6, 6, 2);
-		addValue(6, 7, 8);
-		//Row 8
-		addValue(7, 3, 4);
-		addValue(7, 4, 1);
-		addValue(7, 5, 9);
-		addValue(7, 8, 5);
-		//Row 9
-		addValue(8, 4, 8);
-		addValue(8, 7, 7);
-		addValue(8, 8, 9);
-	}
-
-	/* Rendering */
-	function renderGame (array) {
-		for (var i = 0; i < 9; i++) {
-			for (var j = 0; j < 9; j++) {
-				var value = array[i][j];
-				if ( value !== -1 ) {
-					var cell = getCellById(i, j);
-					cell.addClass('locked');
-					cell.text(value);
-					cell.attr('data-value', value);
-				}
-			}
-		}
-	}
-
-	function markError (rowIndex, colIndex) {
-		$(currentCell).css('color', 'red');
-	}
-
-	function markOk (rowIndex, colIndex) {
-		$(currentCell).css('color', 'black');
-	}
-
-	function getCellById (row, col) {
-		return $('#cell-' + row + '-' + col);
-	}
-
-	function renderNewTime (timeString) {
-		$('#time-input').text(timeString);
-	}
-
-	/* Time and scoring */
+	/* ****************************
+		Time and scoring
+	* ****************************/
 	function startTime () {
 		timer = setInterval(function () {
 			updateTimer();
